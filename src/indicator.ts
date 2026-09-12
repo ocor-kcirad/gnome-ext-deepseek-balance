3cc664d18cf7f38a78c51cb141ad4465bf30f876
ocor-kcirad <dakoyroco@gmail.com>
2026-09-12 11:47:46 +0800

    revert: remove vertical separator in status row

--- src/indicator.ts
+++ src/indicator.ts
@@ -26,7 +26,6 @@ export class UsageIndicator extends PanelMenu.Button {
 
     private readonly panelLabel: St.Label;
     private readonly statusIcon: St.Icon;
-    private readonly separator: St.Widget;
     private readonly statusTooltip: Tooltip;
     private readonly totalValueLabel: St.Label;
     private readonly totalTooltip: Tooltip;
@@ -48,7 +47,6 @@ export class UsageIndicator extends PanelMenu.Button {
         this.updateIcons();
         this.colorSchemeId = this.stSettings.connect('notify::color-scheme', () => {
             this.updateIcons();
-            this.updateSeparator();
         });
 
         this.panelLabel = new St.Label({
@@ -66,13 +64,6 @@ export class UsageIndicator extends PanelMenu.Button {
             style_class: 'deepseek-status-icon',
         });
         statusItem.add_child(this.statusIcon);
-
-        this.separator = new St.Widget({
-            y_expand: true,
-            style_class: 'deepseek-vseparator',
-        });
-        statusItem.add_child(this.separator);
-        this.updateSeparator();
 
         const linkButton = new St.Button({
             child: this.linkIcon,
@@ -144,12 +135,6 @@ export class UsageIndicator extends PanelMenu.Button {
                 `${this.iconsDir}/deepseek-${suffix}.svg`
             ) as unknown as St.Icon['gicon']
         );
-    }
-
-    private updateSeparator(): void {
-        const suffix =
-            this.stSettings.color_scheme === St.SystemColorScheme.PREFER_LIGHT ? 'light' : 'dark';
-        this.separator.set_style_class_name(`deepseek-vseparator deepseek-vseparator-${suffix}`);
     }
 
     private updateUpdatedLabel(): void {
